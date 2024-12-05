9542b4a3511f9023aa8dfa456e4d76927bf2a540
Rittiphon Phoarun <miw1513@hotmail.com>
2024-12-05 16:47:09 +0700

    add github action

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    dotenv().ok();
+    let greeting = env::var("GREETING").unwrap_or_else(|_| "Hello, World!".to_string());
+    HttpResponse::Ok().body(greeting)
 }
 
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
